03f7e7ffad5db98568cc4ba1107af2a2629ed5a4
Uki Aula Fauzi <ukiaula39@gmail.com>
2025-05-31 08:00:58 +0700

    use relative path for service worker registration

--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -21,7 +21,7 @@ export async function registerServiceWorker() {
   }
 
   try {
-    await navigator.serviceWorker.register('/sw.bundle.js');
+    await navigator.serviceWorker.register('sw.bundle.js');
   } catch (error) {
     //
   }
